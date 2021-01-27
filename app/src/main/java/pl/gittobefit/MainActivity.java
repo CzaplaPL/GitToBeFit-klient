3f441d7001c04bfea16e0eaa93f4be7c05156dfd
kwiatkowskiw <kwiatkowskiww@gmail.com>
2021-01-27 14:51:26 +0100

    Zmiana Layoutów v3

--- app/src/main/java/pl/gittobefit/MainActivity.java
+++ app/src/main/java/pl/gittobefit/MainActivity.java
@@ -2,40 +2,21 @@ package pl.gittobefit;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputLayout;
-
-import java.util.Collections;
-
-import pl.gittobefit.dialog.RemindPasswoedDialog;
-import pl.gittobefit.network.ConnectionToServer;
-import pl.gittobefit.user.User;
+import com.google.android.material.navigation.NavigationView;
 
 /***
  * author:Dominik
@@ -76,9 +57,7 @@ public class MainActivity extends AppCompatActivity
 
 
         //chowanie actionBara
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.topAppBar);
-        setSupportActionBar(myToolbar);
-        myToolbar.setNavigationOnClickListener(v -> openDrawer(drawerLayout));
+
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.hide();
