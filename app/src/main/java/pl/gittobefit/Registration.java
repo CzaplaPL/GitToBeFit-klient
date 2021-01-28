ded67a25cb6ad773d3f1103a0c74e915840b8090
kwiatkowskiw <kwiatkowskiww@gmail.com>
2021-01-28 18:33:42 +0100

    Poprawa Logowanie Facebooka

--- app/src/main/java/pl/gittobefit/Registration.java
+++ app/src/main/java/pl/gittobefit/Registration.java
@@ -10,7 +10,7 @@ public class Registration extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        //setContentView(R.layout.activity_registration);
         //chowanie actionBara
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
